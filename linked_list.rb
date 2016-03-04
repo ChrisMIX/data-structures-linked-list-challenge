500715b8f6e8507a4445ee1d6e4250a2cc920d06
Chris Miklius <cmiklius@gmail.com>
2016-03-04 17:30:02 -0600

    establish basic linked list class and node skeleton

--- linked_list.rb
+++ linked_list.rb
@@ -1,5 +1,24 @@
 require_relative 'node'
 
 class LinkedList
+  def initialize(list)
+    @list = []
+  end
 
+  def insert_first(element)
+    @list.unshift(element)
+  end
+
+  def remove_first
+    @list.shift
+  end
+
+  def insert_last(element)
+    @list << element
+  end
+
+  def remove_last
+    @list.pop!
+  end
 end
+
--- node.rb
+++ node.rb
@@ -1,3 +1,15 @@
 class Node
 
+  def initialize(element)
+    @element = element
+  end
+
+  def insert_after(other_node)
+
+  end
+
+  def remove_after()
+
+  end
+
 end
